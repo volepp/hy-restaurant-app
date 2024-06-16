24c86b2befe145e8c6ec4e5774bb9b87172ee679
Oliver Leppänen <oliverleppanen@gmail.com>
2024-06-16 15:28:47 +0300

    Add CSRF protection

--- app/app.py
+++ app/app.py
@@ -1,9 +1,13 @@
 import os
 from flask import Flask
+from flask_wtf import CSRFProtect
+import secrets
 
 ROOT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../")
 app=Flask(__name__, 
           template_folder=os.path.join(ROOT_DIR, "templates"), 
           static_folder=os.path.join(ROOT_DIR, "static")) 
+app.secret_key = secrets.token_hex(16)
+csrf = CSRFProtect(app)
 
 import routes
